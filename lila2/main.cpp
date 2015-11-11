f849b899f85d0229497aaed7355faa0f4da9e78c
tshandan <496509048@qq.com>
2015-11-11 21:08:56 +0800

    hard3
    进一步说明＊是间接取值符，指针指向所指文件地址中的值。

--- lila2/main.cpp
+++ lila2/main.cpp
@@ -11,5 +11,15 @@
 int main(int argc, const char * argv[]) {
     // insert code here...
     std::cout << "Hello, World!\n";
+    int a[4];
+    int *p;
+    a[0]=12;
+    a[1]=15;
+    a[2]=25;
+    a[3]=55;
+    p=a;
+    printf("&a[0]=%x &a[1]=%x  &a[2]=%x  &a[3]=%x a=%x *p=%x\n",a[0],a[1],a[2],a[3],a,p);
+    printf("p[0]=%d p[1]=%d  p[2]=%d p[3]=%d a=%d *p=%d\n",p[0],p[1],p[2],p[3],a,p);
+
     return 0;
 }
