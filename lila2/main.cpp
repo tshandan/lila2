026e4f841517cf9d1ea1799120eebc9e8daaa2f6
tshandan <496509048@qq.com>
2015-11-11 20:21:12 +0800

    Initial Commit

---
+++ lila2/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  lila2
+//
+//  Created by shandan on 15/11/11.
+//  Copyright (c) 2015年 shandan. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
